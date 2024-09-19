72c7076c7c83ef550adfd3162b1b00877c08e774
abnerfilipe7 <abnersousa124@gmail.com>
2024-09-19 13:52:49 -0300

    código

---
+++ PedraPapelTesoura/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+﻿Random random = new Random();
+        string[] opções = { "pedra", "papel", "tesoura" };
+
+        while (true)
+        {
+            Console.Write("Escolha (Pedra, Papel ou Tesoura ): ");
+            string escolhaJogador = Console.ReadLine().ToLower();
+
+           
+
+            if (Array.IndexOf(opções, escolhaJogador) == -1)
+            {
+                Console.WriteLine("Escolha inválida. Tente novamente.");
+                continue;
+            }
+
+            string escolhaComputador = opções[random.Next(opções.Length)];
+            Console.WriteLine($"Computador escolheu: {escolhaComputador}");
+
+            if (escolhaJogador == escolhaComputador)
+                Console.WriteLine("Empate!");
+            else if ((escolhaJogador == "pedra" && escolhaComputador == "tesoura") ||
+                     (escolhaJogador == "papel" && escolhaComputador == "pedra") ||
+                     (escolhaJogador == "pesoura" && escolhaComputador == "papel"))
+                Console.WriteLine("Você ganhou!");
+            else
+                Console.WriteLine("Você perdeu!");
+
+            Console.WriteLine();
+        }
+
